15b996f476b803a810ea69392b0679b0247b6523
Kamila Bektaeva <kabe1222@netcracker.com>
2024-01-10 10:43:36 +0600

    updated webinar2

---
+++ webinar2/HW/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// Console.WriteLine("Enter number");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 7 ==0 && number % 23 == 0)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+// Console.WriteLine("Enter X number");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Y number");
+// int Y = Convert.ToInt32(Console.ReadLine());
+
+// int quarter = 0;
+
+// if (X > 0 && Y > 0)
+// {
+//     quarter = quarter + 1;
+// }
+// if (X > 0 && Y < 0)
+// {
+//     quarter = quarter + 4;
+// }
+// if (X < 0 && Y > 0)
+// {
+//     quarter = quarter + 2;
+// }
+// if (X < 0 && Y < 0)
+// {
+//     quarter = quarter + 3;
+// }
+// Console.WriteLine(quarter);
+
+
+// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// int num  = new Random().Next(10, 100);
+// Console.WriteLine(num);
+
+// int num1 = num/ 10;
+// int num2 = num% 10;
+
+// int max = num1 > num2 ? num1 : num2;
+// Console.WriteLine(max);
+
+
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+
+Console.WriteLine("Enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+Console.WriteLine(N);
+}
+else
+{
+while (N > 0)
+{
+int currentDigit = N % 10;
+N /= 10;
+if (N > 0)
+{
+Console.Write(currentDigit + ",");
+}
+else
+{
+Console.WriteLine(currentDigit);
+}
+}
+}
---
+++ webinar2/Program.cs
@@ -0,0 +1,97 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+// Console.WriteLine("Enter number");
+
+// // int num = int.Parse(Console.ReadLine());
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 =  num/100;  
+// int num3 = num% 10;   
+
+// int result = num1*10+num3;  
+// System.Console.WriteLine(result);
+
+
+
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+// int num  = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+
+// int num2 =  num/10 %10;  
+// int num3 = num% 10;   
+// int result =1;
+
+// while(num3>0)
+// {
+//     result = result*num2;
+//     num3= num3 -1;
+// }
+
+// System.Console.WriteLine(result);
+
+
+
+
+// Напишите программу, которая будет принимать на вход два
+// числа и выводить, является ли второе число кратным первому.
+// Если второе число некратно первому, то программа выводит
+// остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+// Console.WriteLine("Enter number");
+// // int num = int.Parse(Console.ReadLine());
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter number");
+// // int num = int.Parse(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 % num2==0)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine($"No, {num1% num2}");
+// }
+
+
+
+
+// Напишите программу, которая выводит третью с конца
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 456 => 6
+// 7812 => 8
+// 91 => Третьей цифры нет
+
+
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>=100)
+{
+    int result= num /100 %10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("No third number");
+}
